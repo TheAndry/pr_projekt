293cd155b98543ed5b2820fb6b464d2e6ff3b7b0
unknown <mart99in.server@gmail.com>
2018-11-15 10:56:53 +0200

    Lisa: .csv faili salvestus ja lugemine

--- rahavood.py
+++ rahavood.py
@@ -1,9 +1,25 @@
 from tkinter import *
+import csv
 
-def salvesta():
-    andmed[len(andmed)+1]=set((e1.get(), e2.get()))
-
-andmed = {}
+def add():
+    data[len(data)+1]=set((e1.get(), e2.get()))
+    
+def save():
+    w = csv.DictWriter(open("raha_data.csv","w"), ['nr','sisu'])
+    w.writeheader()
+    for nr, sisu in data.items():
+        rida = {}
+        rida['nr']=nr
+        rida['sisu']=sisu
+        w.writerow(rida)
+        
+def read_data():
+    w = csv.DictReader(open("raha_data.csv","r"))
+    for rida in w:
+        data[rida['nr']]=rida['sisu']
+        
+data = {}
+read_data()
 
 main = Tk()
 main.title("Projekt")
@@ -18,6 +34,7 @@ e2 = Entry(main)
 e1.grid(row=0, column=1)
 e2.grid(row=0, column=3)
 
-Button(main, text='Lisa', command=salvesta).grid(row=0, column=4, sticky=W)
+Button(main, text='Lisa', command=add).grid(row=0, column=4, sticky=W)
+Button(main, text='Salvesta andmed', command=save).grid(row=0, column=5)
 
 main.mainloop()
